62a7c64bec31fd49186b74f0db647d9e81c84e11
v7lin <v7lin@qq.com>
2019-03-03 22:32:55 +0800

    xxx

---
+++ FakeMobileAnalytics/Classes/FakeMobileAnalytics.h
@@ -0,0 +1,16 @@
+//
+//  FakeMobileAnalytics.h
+//  FakeMobileAnalytics
+//
+//  Created by v7lin on 2019/3/3.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FakeMobileAnalytics : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FakeMobileAnalytics/Classes/FakeMobileAnalytics.m
@@ -0,0 +1,12 @@
+//
+//  FakeMobileAnalytics.m
+//  FakeMobileAnalytics
+//
+//  Created by v7lin on 2019/3/3.
+//
+
+#import "FakeMobileAnalytics.h"
+
+@implementation FakeMobileAnalytics
+
+@end
